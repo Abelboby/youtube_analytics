ded5009e74c1ab0862c50eb18d60b1e5a2f19290
Abelboby <abeldavisboby@gmail.com>
2024-12-29 11:59:41 +0530

    updated dockerfile

--- app/utils.py
+++ app/utils.py
@@ -2,8 +2,12 @@ import psutil
 import os
 
 def check_memory_usage():
+    """
+    Get memory usage information in a lightweight way
+    """
     process = psutil.Process(os.getpid())
+    memory_info = process.memory_info()
     return {
-        'memory_usage_mb': process.memory_info().rss / 1024 / 1024,
-        'memory_percent': process.memory_percent()
+        'memory_usage_mb': memory_info.rss / 1024 / 1024,  # Convert to MB
+        'memory_percent': process.memory_percent(memtype='rss')
     }
